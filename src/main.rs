46da2d6c7740bdda2fb9970733274bae08f11225
Jay Kickliter <jay@kickliter.com>
2023-07-29 11:54:03 -0600

    Gen world map

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,13 @@ use anyhow::{anyhow, Result};
 use byteorder::{LittleEndian as LE, ReadBytesExt, WriteBytesExt};
 use clap::Parser;
 use flate2::read::GzDecoder;
+use geojson::{Feature, FeatureCollection, GeoJson, JsonObject};
+use h3o::{
+    geom::{Geometry, ToCells},
+    CellIndex, Resolution,
+};
 use hextree::{disktree::DiskTree, Cell, HexTreeMap};
+use serde_json::Value;
 use std::{
     fs::File,
     io::{Seek, SeekFrom},
@@ -21,6 +27,17 @@ enum Cli {
         sets: Vec<PathBuf>,
     },
 
+    /// Generate a disktree from source h3idz sets
+    GenWorld {
+        /// Resolution to use for h3 cells
+        #[arg(default_value_t = Resolution::Seven, short, long)]
+        resolution: Resolution,
+        /// Output file
+        out: PathBuf,
+        /// Input h3idz files
+        world: PathBuf,
+    },
+
     /// Lookup target H3 cell
     Lookup {
         /// On disk HexTreeMap
@@ -30,9 +47,66 @@ enum Cli {
     },
 }
 
+fn to_cells(
+    idx: u8,
+    feature: Feature,
+    resolution: Resolution,
+) -> Result<(u8, JsonObject, Vec<CellIndex>)> {
+    println!("generating {:?}", feature.properties);
+    let start = std::time::Instant::now();
+    let properties = feature
+        .properties
+        .ok_or_else(|| anyhow!("no properties for feature {idx}"))?;
+    let geometry = feature
+        .geometry
+        .ok_or_else(|| anyhow!("feature {idx} missing geometry"))?;
+    let geometry = Geometry::try_from(&geometry)?;
+    let cells = geometry.to_cells(resolution).collect();
+    println!("  generated {:?} in {:?}", properties, start.elapsed());
+    Ok((idx, properties, cells))
+}
+
+fn compact_cells(cells: Vec<CellIndex>) -> Result<Vec<CellIndex>> {
+    let compacted = CellIndex::compact(cells)?;
+    Ok(compacted.collect())
+}
+
 impl Cli {
     fn run(self) -> Result<()> {
         match self {
+            Cli::GenWorld {
+                resolution,
+                out,
+                world,
+            } => {
+                let json = GeoJson::from_reader(File::open(world)?)?;
+                let mut disktree_file = File::create(out)?;
+                let world = FeatureCollection::try_from(json)?;
+
+                let mut world_map: HexTreeMap<u8> = HexTreeMap::new();
+                let mut property_lut = Vec::new();
+
+                for (idx, feature) in world.features.into_iter().enumerate() {
+                    let idx = u8::try_from(idx)?;
+                    let (_, properties, plain_cells) = to_cells(idx, feature, resolution)?;
+                    let compact_cells = compact_cells(plain_cells)?;
+                    for cell in compact_cells {
+                        let cell = hextree::Cell::from_raw(cell.into())?;
+                        world_map.insert(cell, idx);
+                    }
+                    let properties = Value::Object(properties);
+                    property_lut.push(properties.to_string());
+                }
+
+                world_map.to_disktree(&mut disktree_file, |wtr, &val| wtr.write_u8(val))?;
+
+                // Append country LuT to end of `out` and write
+                // its position the end of the file.
+                let property_lut_pos = disktree_file.seek(SeekFrom::End(0))?;
+                bincode::serialize_into(&mut disktree_file, &property_lut)?;
+                disktree_file.write_u64::<LE>(property_lut_pos)?;
+            }
+
             Cli::Generate { out, sets } => {
                 // [(Region, Input File), ..]
                 let inputs = {
@@ -69,7 +143,6 @@ impl Cli {
                         region_map.insert(Cell::try_from(entry)?, n as u8);
                     }
                 }
-
                 // Create an array of region names that we derive from
                 // the input files base names.
                 let region_name_lut: Vec<&str> =
